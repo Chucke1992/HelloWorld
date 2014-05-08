c23cc8e368ba62c4cf4a06e87b3268c1dda13b18
Chucke1992 <artemii1992@yahoo.com>
2014-05-08 11:25:46 +0300

    Update MainClass.java
    Added method calculate

--- GitHelloWorld/src/com/project/main/MainClass.java
+++ GitHelloWorld/src/com/project/main/MainClass.java
@@ -4,9 +4,15 @@ import com.project.test.SimpleClass;
 
 public class MainClass {
 
+	public static int calculate(int a, int b) {
+		return a + b;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Message from gitHub folder: " + SimpleClass.getMessage());
 		System.out.println("Another line");
+		
+		System.out.println("Method :" + calculate(5, 6));
 	}
 	
 }
