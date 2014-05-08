88e586efc97e4642873b99945b2a2408b2123636
Chucke1992 <artemii1992@yahoo.com>
2014-05-08 11:20:11 +0300

    update MainClass

--- GitHelloWorld/src/com/project/main/MainClass.java
+++ GitHelloWorld/src/com/project/main/MainClass.java
@@ -5,7 +5,7 @@ import com.project.test.SimpleClass;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println(SimpleClass.getMessage());
+		System.out.println("Message: " + SimpleClass.getMessage());
 	}
 	
 }
