394c4acc7932d83283060b59d90274a4a24bf1d0
Chucke1992 <artemii1992@yahoo.com>
2014-05-08 12:21:00 +0300

    Updated MainClass

--- GitHelloWorld/src/com/project/main/MainClass.java
+++ GitHelloWorld/src/com/project/main/MainClass.java
@@ -2,6 +2,7 @@ package com.project.main;
 
 import com.project.test.AnotherSimpleClass;
 import com.project.test.SimpleClass;
+import com.project.test.UltraClass;
 
 public class MainClass {
 
@@ -15,6 +16,7 @@ public class MainClass {
 		System.out.println("Method :" + calculate(5, 6));
 		System.out.println("Class method 1: " + SimpleClass.calculate(5, 6));
 		System.out.println("Class method 2: " + AnotherSimpleClass.calculate(5, 6));
+		System.out.println("Class method 3: " + UltraClass.calculate(5, 6));
 	}
 	
 }
