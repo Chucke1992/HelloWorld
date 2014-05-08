ea391cd6cedbca920a900a3acc7bfb717b399e6c
Chucke1992 <artemii1992@yahoo.com>
2014-05-08 14:22:16 +0300

    Modified MainClass.java

--- GitHelloWorld/src/com/project/main/MainClass.java
+++ GitHelloWorld/src/com/project/main/MainClass.java
@@ -2,13 +2,14 @@ package com.project.main;
 
 import com.project.test.AnotherSimpleClass;
 import com.project.test.SimpleClass;
-import com.project.test.UltraClass;
 
 public class MainClass {
 
 	public static int calculate(int a, int b) {
 		return a + b;
 	}
+	
+	public static void nothing() {};
 
 	public static void main(String[] args) {
 		System.out.println("Message from gitHub folder: " + SimpleClass.getMessage());
@@ -16,8 +17,6 @@ public class MainClass {
 		System.out.println("Method :" + calculate(5, 6));
 		System.out.println("Class method 1: " + SimpleClass.calculate(5, 6));
 		System.out.println("Class method 2: " + AnotherSimpleClass.calculate(5, 6));
-		System.out.println("Class method 3: " + UltraClass.calculate(5, 6));
-		//no data
 		
 		System.out.println("Dater!");
 	}
