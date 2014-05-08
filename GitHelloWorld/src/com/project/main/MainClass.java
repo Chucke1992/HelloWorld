37a69120effc5996258e12e4be0043e38110e549
Chucke1992 <artemii1992@yahoo.com>
2014-05-08 11:24:07 +0300

    updated MainClass.java

--- GitHelloWorld/src/com/project/main/MainClass.java
+++ GitHelloWorld/src/com/project/main/MainClass.java
@@ -6,6 +6,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Message from gitHub folder: " + SimpleClass.getMessage());
+		System.out.println("Another line");
 	}
 	
 }
